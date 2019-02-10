c964a7780721f9e4132a704081ef9eb2f53536b6
Phil Crow <crow.phil@gmail.com>
2019-02-10 07:52:29 -0600

    compile script makes build dir if needed

--- compilebook.sh
+++ compilebook.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 lilypond-book --output=build --pdf book.lytex
+
+# make sure there is a build directory
+if [ ! -d "build" ]; then
+    mkdir build
+fi
 cd build
+
 pdflatex book
